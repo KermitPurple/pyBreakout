ca359731778299b5614e90375f36a7ca31338916
Shane McDonough <shanemcdman@gmail.com>
2020-03-12 13:31:35 -0700

    Create loop

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
 pygame.display.init()
 size = 900,600
 screen = pygame.display.set_mode(size)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
