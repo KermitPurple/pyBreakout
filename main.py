6a0d81b83bbd99c886cac1298c1700b2ae921906
Shane McDonough <shanemcdman@gmail.com>
2020-03-12 13:44:39 -0700

    Create a main function

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pygame
 from ball import Ball
 import os
 
-os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
-pygame.display.init()
-size = 900,600
-screen = pygame.display.set_mode(size)
+def main():
+    os.environ["SDL_VIDEO_WINDOW_POS"] = "15,30"
+    pygame.display.init()
+    size = 900,600
+    screen = pygame.display.set_mode(size)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+if __name__ == "__main__":
+    main()
