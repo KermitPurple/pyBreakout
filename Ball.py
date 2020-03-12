86482de3a7ca6ea88b6445ce1645771d5b03eb80
Shane McDonough <shanemcdman@gmail.com>
2020-03-12 15:11:24 -0700

    Create grid bounce

--- Ball.py
+++ Ball.py
@@ -19,7 +19,12 @@ class Ball:
             self.vel = (self.vel[0], -self.vel[1])
 
     def collidegrid(self, grid):
-        pass
+        for i, line in enumerate(grid.blocks):
+            for j, block in enumerate(line):
+                if block:
+                    if self.pos[0] + self.r >= j * grid.width and self.pos[0] - self.r <= (j + 1) * grid.width: # if x coord is in block
+                        if self.pos[1] + self.r >= i * grid.height and self.pos[1] - self.r <= (i + 1) * grid.height: # if y coord is in block
+                            self.vel = (self.vel[0], -self.vel[1])
 
     def collidepaddle(self, paddle):
         pass
