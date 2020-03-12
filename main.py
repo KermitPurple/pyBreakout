cc5634c7ffbd9c5e4002275c555e2af1c3e755d7
Shane McDonough <shanemcdman@gmail.com>
2020-03-12 14:52:05 -0700

    Print grid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from ball import Ball
+from grid import Grid
 import os
 
 def main():
@@ -8,15 +9,17 @@ def main():
     size = 900,600
     screen = pygame.display.set_mode(size)
     ball = Ball(screen, (size[0]/2,size[1]/2), (0.5,0.5))
+    grid = Grid(screen, size)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0,0,0))
-        ball.update()
-        ball.collide(size)
-        ball.draw()
+        #ball.update()
+        #ball.collide(size)
+        #ball.draw()
+        grid.draw()
         pygame.display.update()
 
 if __name__ == "__main__":
