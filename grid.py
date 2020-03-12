0419e8b250fef4c93d82ebcff344719beae42f5d
Shane McDonough <shanemcdman@gmail.com>
2020-03-12 15:02:18 -0700

    rename grid.block to grid.blocks

--- grid.py
+++ grid.py
@@ -5,13 +5,13 @@ class Grid:
         self.screen = screen
         self.size = size
         self.scale = (10,10)
-        self.block = [[True] * self.scale[0]] * self.scale[1]
+        self.blocks = [[True] * self.scale[0]] * self.scale[1]
         self.width = self.size[0]/self.scale[0]
         self.height = self.size[1]/self.scale[1]/2
 
     def draw(self):
         colors = [(255,0,0),(255,90,0), (255,230,0), (43, 255, 0), (0,255,255), (0,0,255), (140,0,255),(225,0,255)]
-        for i,line in enumerate(self.block):
+        for i,line in enumerate(self.blocks):
             for j,block in enumerate(line):
                 if block:
                     color = colors[(i + j) % len(colors)]
