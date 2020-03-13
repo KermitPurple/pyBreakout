2ee4eb47e01599960bb87f05b324450e74f03050
Shane McDonough <shanemcdman@gmail.com>
2020-03-12 23:40:27 -0700

    Change collision to account for speed of paddle

--- Ball.py
+++ Ball.py
@@ -36,7 +36,7 @@ class Ball:
     def collidepaddle(self, paddle):
         if self.pos[0] + self.r > paddle.pos[0] and self.pos[0] - self.r < paddle.pos[0] + paddle.length: #inside x
             if self.pos[1] + self.r > paddle.pos[1] and self.pos[1] - self.r < paddle.pos[1] + paddle.height: #inside y
-                self.vel = (self.vel[0], -self.vel[1])
+                self.vel = (self.vel[0] + paddle.vel[0]/15, -self.vel[1])
 
     def update(self):
         self.pos = (self.pos[0] + self.vel[0], self.pos[1] + self.vel[1])
